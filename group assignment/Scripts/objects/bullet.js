13d2f996d81386af7d32c045d6cf87fc1e03b483
mike <a>
2019-02-04 13:48:04 -0500

    test

---
+++ group assignment/Scripts/objects/bullet.js
@@ -0,0 +1,19 @@
+var objects;
+(function (objects) {
+    var bullet = /** @class */ (function () {
+        // Variables
+        // Constructor
+        function bullet(boolean) {
+            /*
+        this.regX = this.getBounds().width * 0.5;
+        this.regY = this.getBounds().height * 0.5;
+
+        this.x = x;
+        this.y = y;
+        */
+        }
+        return bullet;
+    }());
+    objects.bullet = bullet;
+})(objects || (objects = {}));
+//# sourceMappingURL=bullet.js.map
