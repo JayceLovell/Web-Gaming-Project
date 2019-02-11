1ba1157759958df253280948ee207308a94d769e
yueyang <yueyang>
2019-02-11 14:43:42 -0500

    added gameobject, ghost and some of player script

--- group assignment/Scripts/objects/ghost.ts
+++ group assignment/Scripts/objects/ghost.ts
@@ -1,32 +1,37 @@
 module objects {
-    export class ghost extends Image {
+    export class ghost extends GameObject {
         // Variables
         private forward: boolean =true;
         private xmin:number;
         private xmax:number;
         private ymin:number;
         private ymax:number;
-        private speed:number;
         // Constructor
-        constructor(assetManager: createjs.LoadQueue, imageString:string, x:number = 0, y:number = 0,xmin:number,xmax:number, ymin:number,ymax:number,speed:number) {
-            // super(imageString);
-            super(assetManager,imageString,x,y);   // Actual loaded object is of type BLOB  
+        constructor(assetManager: createjs.LoadQueue, imageString:string, x:number = 0, y:number = 0,xmin:number,xmax:number, ymin:number,ymax:number) {
+            super(assetManager,imageString); 
             this.xmin=xmin;
             this.xmax=xmax;
             this.ymin=ymin;
             this.ymax=ymax;
-            this.speed=sped;
+            this.x=x;
+            this.y=y;
         }
         // Methods
 
-        public move(newX : number)
+        public Update()
         {
-            if(forward){
-
+            if(this.forward){
+                this.speedX = Math.abs(this.xmax-this.x);
+                this.speedY = Math.abs(this.ymax-this.y);
+                if(Math.abs(this.xmax-this.x)<1&&Math.abs(this.ymax-this.y)<1)
+                    this.forward=false;
             }else{
-
+                this.speedX = Math.abs(this.xmin-this.x);
+                this.speedY = Math.abs(this.ymin-this.y);
+                if(Math.abs(this.xmin-this.x)<1&&Math.abs(this.ymin-this.y)<1)
+                    this.forward=true;
             }
-            if(this.getX())
+
         }
 
 
