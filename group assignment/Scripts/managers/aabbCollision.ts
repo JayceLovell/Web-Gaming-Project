392e3775a0c175687d86688e1b52bf50cb6e26ac
aldrin312 <aldrin_fernandez312@yahoo.com>
2019-02-25 09:44:55 -0500

    implemented aabb collision scripts

---
+++ group assignment/Scripts/managers/aabbCollision.ts
@@ -0,0 +1,34 @@
+module managers {
+    export class AABBCollisions {
+        private static explodeSFX:createjs.AbstractSoundInstance
+
+        public static Check(obj1: objects.GameObject, obj2: objects.GameObject):boolean {
+
+
+            let leftPointObj1 = obj1.x -obj1.halfW;
+            let rightPointObj1 = obj1.x+ obj1.halfW;
+
+            let leftPointObj2 = obj2.x-obj2.halfW;
+            let rightPointObj2 = obj2.x + obj2.halfW;
+
+            let upPointObj1 = obj1.y -obj1.halfH;
+            let downPointObj1 = obj1.y+ obj1.halfH;
+
+            let upPointObj2 = obj2.y-obj2.halfH;
+            let downPointObj2 = obj2.y + obj2.halfH;
+
+            if(((leftPointObj1 <= rightPointObj2 && leftPointObj1>= leftPointObj2) || (rightPointObj1 >= leftPointObj2 && rightPointObj1 <= rightPointObj2))&&((upPointObj1<=downPointObj2&&upPointObj1>=upPointObj2)||(downPointObj1>=upPointObj2 && downPointObj1<=downPointObj2))){
+                return true;
+
+            }else {
+                return false;
+
+            }
+
+
+
+        }
+
+
+    }
+}
---
+++ group assignment/Scripts/objects/ghost.js
@@ -0,0 +1,52 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var objects;
+(function (objects) {
+    var ghost = /** @class */ (function (_super) {
+        __extends(ghost, _super);
+        // Constructor
+        function ghost(assetManager, imageString, x, y, xmin, xmax, ymin, ymax) {
+            if (x === void 0) { x = 0; }
+            if (y === void 0) { y = 0; }
+            var _this = _super.call(this, assetManager, imageString) || this;
+            // Variables
+            _this.forward = true;
+            _this.xmin = xmin;
+            _this.xmax = xmax;
+            _this.ymin = ymin;
+            _this.ymax = ymax;
+            _this.x = x;
+            _this.y = y;
+            return _this;
+        }
+        // Methods
+        ghost.prototype.Update = function () {
+            if (this.forward) {
+                this.speedX = Math.abs(this.xmax - this.x);
+                this.speedY = Math.abs(this.ymax - this.y);
+                if (Math.abs(this.xmax - this.x) < 1 && Math.abs(this.ymax - this.y) < 1)
+                    this.forward = false;
+            }
+            else {
+                this.speedX = Math.abs(this.xmin - this.x);
+                this.speedY = Math.abs(this.ymin - this.y);
+                if (Math.abs(this.xmin - this.x) < 1 && Math.abs(this.ymin - this.y) < 1)
+                    this.forward = true;
+            }
+        };
+        return ghost;
+    }(objects.GameObject));
+    objects.ghost = ghost;
+})(objects || (objects = {}));
+//# sourceMappingURL=ghost.js.map
