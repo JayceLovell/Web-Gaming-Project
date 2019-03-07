b3bbefa19c72dafbb5998c9dc0092baddc5e622c
aldrin312 <aldrin_fernandez312@yahoo.com>
2019-03-07 17:49:44 -0500

    addedd keyboard control and player gravity

--- group assignment/Scripts/objects/player.ts
+++ group assignment/Scripts/objects/player.ts
@@ -1,6 +1,9 @@
 module objects {
     export class Player extends objects.GameObject {
         // Variables
+        public isDead:boolean;
+        public speed:number;
+        public vSpeed:number;
         // Constructor
         constructor(assetManager:createjs.LoadQueue) {
             super(assetManager, "player");
@@ -9,17 +12,49 @@ module objects {
         // Methods / functions
         public Start():void {
             this.y = 700;
+
+            this.isDead = false;
+ 
+            this.x = 100;
+            this.speed = 5;
+            this.vSpeed = 1;
         }
 
         public Update():void {
             this.Move();
             this.CheckBounds();
+
+            if(this.vSpeed < 5){
+                this.vSpeed +=0.12;
+
+            }
+            this.y += this.vSpeed;
         }
 
         public Reset():void {}
 
         public Move():void {
-            this.x = objects.Game.stage.mouseX; // objects.Game.stage is a global variable
+            //this.x = objects.Game.stage.mouseX; // objects.Game.stage is a global variable
+
+            if(objects.Game.keyboardManager.moveLeft){
+                this.x -= this.speed;
+            }
+
+            if(objects.Game.keyboardManager.moveRight){
+                this.x += this.speed;
+            }
+
+            if(objects.Game.keyboardManager.moveUp){
+                //this.y -= 1;
+                // if(this.vSpeed >0){
+                //     this.vSpeed *= -1;
+
+                // }
+            }
+
+            if(objects.Game.keyboardManager.moveDown){
+                this.y += this.speed;
+            }
         }
 
         public CheckBounds():void {
