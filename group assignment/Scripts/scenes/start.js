2be01363b3483d149ca8b9caeaedcfaa35ab7853
deruip <aronly1997@hotmail.com>
2019-03-15 15:56:27 -0400

    Resize 2: Resize Harder

--- group assignment/Scripts/scenes/start.js
+++ group assignment/Scripts/scenes/start.js
@@ -27,10 +27,10 @@ var scenes;
         };
         // Use start function to create objects
         StartScene.prototype.Start = function () {
-            this.welcomeLabel = new objects.Label("Miner Horror Never Ending Cause \n He  Failed To Win A Race In \n Part Two and Died In Part 1 \n So Now He Is in part three \n fighting for his life in hell \n against an annoying precision \n platformer", "32px", "Consolas", "#FFFFFF", 1800, 140, true);
-            this.buttonLabel = new objects.Label("Start Game", "30px", "Consolas", "#FFFFFF", 320, 387, true);
-            this.startButton = new objects.Button(this.assetManager, "startButton", 320, 400);
-            this.bgImage = new objects.Image(this.assetManager, "backGroundImageMain", 320, 400);
+            this.welcomeLabel = new objects.Label("Miner Horror Never Ending Cause \n He  Failed To Win A Race In \n Part Two and Died In Part 1 \n So Now He Is in part three \n fighting for his life in hell \n against an annoying precision \n platformer", "32px", "Consolas", "#FFFFFF", 1920, 140, true);
+            this.buttonLabel = new objects.Label("Start Game", "30px", "Consolas", "#FFFFFF", 480, 395, true);
+            this.startButton = new objects.Button(this.assetManager, "startButton", 480, 400);
+            this.bgImage = new objects.Image(this.assetManager, "backGroundImageMain", 480, 400);
             this.Main();
         };
         StartScene.prototype.Update = function () {
